a64c6b1dfc31bc2794b3827b85c87592d7dacf96
joneshf <jones3.hardy@gmail.com>
2015-08-05 06:39:39 -0700

    Extract Pair to its own package

---
+++ src/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var pbp = require('pbp');
+var mixin = require('ce-mixin').mixin;
+var Stringify = require('ce-stringify').Stringify;
+
+var value = pbp.value;
+var enumerableValue = pbp.enumerableValue;
+
+var Pair = function(x, y) {
+  return mixin(Stringify, Object.create(null, {
+    type: value('Pair'),
+    args: value([x, y]),
+    _1: enumerableValue(x),
+    _2: enumerableValue(y),
+  }));
+};
+
+function fst(pair) {
+  return pair._1;
+}
+
+function snd(pair) {
+  return pair._2;
+}
+
+module.exports = {
+  fst: fst,
+  Pair: Pair,
+  snd: snd,
+};
+
+console.log('%s', Pair(3, 4));
